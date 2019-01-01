2099b104a2b1162cd4fe98ba7537c22d4ddaa775
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2019-01-01 12:18:46 -0600

    Invalid initbot function

--- discordpp/bot.hh
+++ discordpp/bot.hh
@@ -21,21 +21,11 @@ namespace discordpp {
         Bot(){
             needInit["Bot"] = true;
         }
-        void initBot(unsigned int apiVersionIn, std::string tokenIn){
-            if(needInit["Bot"]){
-                apiVersion = apiVersionIn;
-                token = tokenIn;
-                aioc = std::make_shared<asio::io_context>();
-                needInit["Bot"] = false;
-            }
-        }
         void initBot(unsigned int apiVersionIn, std::string tokenIn, std::shared_ptr<asio::io_context> aiocIn){
-            if(needInit["Bot"]){
-                apiVersion = apiVersionIn;
-                token = tokenIn;
-                aioc = aiocIn;
-                needInit["Bot"] = false;
-            }
+            apiVersion = apiVersionIn;
+            token = tokenIn;
+            aioc = aiocIn;
+            needInit["Bot"] = false;
         }
     };
 }
