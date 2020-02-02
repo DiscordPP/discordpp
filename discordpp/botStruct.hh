b37b05b2c26eda9c3de375548cce06ce4ed5303c
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2020-02-02 17:00:23 -0600

    Reformat Code

--- discordpp/botStruct.hh
+++ discordpp/botStruct.hh
@@ -11,46 +11,48 @@
 
 #include <nlohmann/json.hpp>
 
-namespace discordpp {
-    using json = nlohmann::json;
+namespace discordpp{
+	using json = nlohmann::json;
 
-    struct ratelimit{
-        int millis;
-    };
+	struct ratelimit{
+		int millis;
+	};
 
-    class BotStruct {
-    public:
-        virtual ~BotStruct(){};
-        virtual json call(std::string requestType, std::string targetURL, json body = {}) = 0;
-        virtual void send(int opcode, json payload = {}) = 0;
+	class BotStruct{
+	public:
+		virtual ~BotStruct(){};
 
-        void run(){
-            bool ready = true;
-            for(auto module: needInit){
-                if(module.second){
-                    std::cerr << "Forgot to initialize: " << module.first << '\n';
-                    ready = false;
-                }
-            }
-            if(ready) {
-                runctd();
-            }
-        }
+		virtual json call(std::string requestType, std::string targetURL, json body = {}) = 0;
 
-    protected:
-        virtual void runctd(){
-            std::cerr << "Starting run loop" << '\n';
-            aioc->run();
-            std::cerr << "Ending run loop" << '\n';
-        }
+		virtual void send(int opcode, json payload = {}) = 0;
 
-        virtual void recievePayload(json payload) = 0;
+		void run(){
+			bool ready = true;
+			for(auto module: needInit){
+				if(module.second){
+					std::cerr << "Forgot to initialize: " << module.first << '\n';
+					ready = false;
+				}
+			}
+			if(ready){
+				runctd();
+			}
+		}
 
-        std::map<std::string, bool> needInit;
-        unsigned int apiVersion = 6;
-        std::shared_ptr<boost::asio::io_context> aioc;
-        std::string token;
-    };
+	protected:
+		virtual void runctd(){
+			std::cerr << "Starting run loop" << '\n';
+			aioc->run();
+			std::cerr << "Ending run loop" << '\n';
+		}
+
+		virtual void recievePayload(json payload) = 0;
+
+		std::map<std::string, bool> needInit;
+		unsigned int apiVersion = 6;
+		std::shared_ptr<boost::asio::io_context> aioc;
+		std::string token;
+	};
 }
 
 #endif //EXAMPLE_BOT_BOTREQUIRED_HH
