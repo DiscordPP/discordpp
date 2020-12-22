1a069eaa6eeb42e0f2c2dfbb04dc0fa5c89ff6de
niansa <anton-sa@web.de>
2020-12-22 21:47:31 +0100

    Added anyHandler

--- discordpp/alias.hh
+++ discordpp/alias.hh
@@ -14,6 +14,7 @@ using json = nlohmann::json;
 template <typename T> using sptr = std::shared_ptr<T>;
 
 using handleEvent = std::function<void(const json msg)>;
+using handleAnyEvent = std::function<void(const std::string& name, const json msg)>;
 using handleRead = std::function<void(const bool error, const json msg)>;
 using handleWrite = std::function<void(const bool error)>;
 using handleSent = std::function<void()>;
