639a8bdd2820aa81478d55362cf864fad8b707f5
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2021-10-04 23:38:17 -0500

    Progress on Objects plugin

--- discordpp/util.hh
+++ discordpp/util.hh
@@ -43,21 +43,41 @@ inline std::string generate_boundary(const std::string &payload,
 } // namespace discordpp
 
 namespace nlohmann {
-
 // https://github.com/nlohmann/json/issues/1749#issuecomment-772996219
 template <class T>
-void to_json(nlohmann::json &j, const std::optional<T> &v) {
-    if (v.has_value())
-        j = *v;
-    else
-        j = nullptr;
-}
+struct adl_serializer<std::optional<T>> {
+    static void to_json(nlohmann::json &j, const std::optional<T> &v) {
+        if(v.has_value()){
+            j = v.value();
+        }else{
+            j = nullptr;
+        }
+    }
 
-template <class T>
-void from_json(const nlohmann::json &j, std::optional<T> &v) {
-    if (j.is_null())
-        v = std::nullopt;
-    else
-        v = j.get<T>();
-}
+    static void from_json(const nlohmann::json &j, std::optional<T> &v) {
+        if (j.is_null()) {
+            v = std::nullopt;
+        }else {
+            v = j.get<T>();
+        }
+    }
+};
+
+template <typename T> struct adl_serializer<std::vector<std::shared_ptr<T>>> {
+    static void to_json(json &j, const std::vector<std::shared_ptr<T>> &v) {
+        j = json::array();
+        for (auto t : v) {
+            j.push_back(*t);
+        }
+    }
+
+    static void from_json(const json &j, std::vector<std::shared_ptr<T>> &v) {
+        if (!j.is_array()) {
+            throw json::type_error::create(317, "j must be an array", j);
+        }
+        for (auto t : j) {
+            v.push_back(std::make_shared<T>(j.get<T>()));
+        }
+    }
+};
 } // namespace nlohmann
